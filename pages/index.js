f3e342d64941778eec78ac3140f13393f8e33add
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-06-16 06:24:42 -0500

    save point

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
+import { BiDotsHorizontalRounded} from 'react-icons/bi'
+import {AiOutlinePlus} from 'react-icons/ai'
+
+
 
 
 const styles = {
@@ -10,46 +14,64 @@ export default function Home() {
       {/* <Header /> */}
       <div className={styles.mainContainer}>
 
-        <div className={styles.portfolioAmountContainer}>
+        <div className={styles.leftMain}> 
+        
+        
 
-          <div className={styles.portfolioAmount}> 88 ETH </div>
+                  <div className={styles.portfolioAmountContainer}>
 
-            <div className={styles.portfolioPercent}>
-                  +0.00008(+0.57%)
-                  <span className={styles.pastHour}>Past Hour</span>
-            </div>
-        </div>
-        <div className="dummyContainer">
+                    <div className={styles.portfolioAmount}> 88 ETH </div>
 
-          <div className={styles.chartContainer}>
-            {/* <PortfolioChart /> */}
-          </div>
-        </div>
+                      <div className={styles.portfolioPercent}>
+                            +0.00008(+0.57%)
+                            <span className={styles.pastHour}>Past Hour</span>
+                      </div>
+                  </div>
+                  <div className="dummyContainer">
 
-        <div className={styles.buyingPowerContainer}>
-            <div className={styles.buyingPowerTitle}>Buying Power</div>
-            <div className={styles.buyingPowerAmount}> 12 ETH</div>
-        </div>
+                    <div className={styles.chartContainer}>
+                      {/* <PortfolioChart /> */}
+                    </div>
+                  </div>
 
-        <div className={styles.notice}>
+                  <div className={styles.buyingPowerContainer}>
+                      <div className={styles.buyingPowerTitle}>Buying Power</div>
+                      <div className={styles.buyingPowerAmount}> 12 ETH</div>
+                  </div>
 
-            <div className={styles.noticeContainer}>
+                  <div className={styles.notice}>
 
-              <div className={styles.noticeTitle}> Send Funds </div>
-              <div className={styles.noticeMessage}>Transfer your funds here.</div>
-              {/* <BuyTokens/> */}
-            </div>
-        </div>
+                      <div className={styles.noticeContainer}>
 
-
-        {/* <Notice /> */}
+                        <div className={styles.noticeTitle}> Send Funds </div>
+                        <div className={styles.noticeMessage}>Transfer your funds here.</div>
+                        {/* <BuyTokens/> */}
+                      </div>
+                  </div>
+                  {/* <Notice /> */}
 
       </div>
 
       <div className={styles.rightMain}>
         <div className={styles.rightMainItem}>
           <div className={styles.ItemTitle}>Crypto Currencies</div>
+          <BiDotsHorizontalRounded className={styles.moreOptions} />
         </div>
+        {/* Map through coins and for each coin, make an assest component
+        
+        <Asset /> */}
+
+        <div className={styles.rightMainItem} >
+
+
+          <div className={styles.ItemTitle}>Lists</div>
+
+          <AiOutlinePlus className={styles.moreOptions} />
+
+
+
+        </div>
+      </div>
       </div>
     </div>
   )
